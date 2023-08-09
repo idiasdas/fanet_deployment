fa92bb64a6b2221942a5a46772e1b8427f10e84e
idiasdas <igordias.ti@gmail.com>
2023-08-09 13:59:11 +0200

    changed trace import to lowercase

--- src/tests/trace_test.py
+++ src/tests/trace_test.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('..')
 from import_file import *
-from Trace import Trace
+from trace import Trace
 
 sensors_trace = Trace(5,3,20,100)
 sensors_trace.plot_trace("out/trace_plot.eps")
