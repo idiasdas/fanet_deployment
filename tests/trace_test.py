53ecd685437b62e9156d5ca2cef34468e3bec004
idiasdas <igordias.ti@gmail.com>
2023-08-10 11:26:34 +0200

    Now the test considers the relative path
    You can run it from anywhere and it will still import the modules from
    src/ correctly and save and read files in tests/out/

--- tests/trace_test.py
+++ tests/trace_test.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('..')
+this_dirctory = __file__.split('trace_test.py')[0]  # directory of trace_test.py
+sys.path.append(this_dirctory + '../src/')          # Now can import src modules
 from import_file import *
 from targets_trace import Trace
 
@@ -13,14 +14,14 @@ print(out_txt.format(variable = "target_speed", test_value = str(sensors_trace.t
 print(out_txt.format(variable = "area_size", test_value = str(sensors_trace.area_size)))
 print(out_txt.format(variable = "time_step_delta", test_value = str(sensors_trace.time_step_delta)))
 
-print(" - Plotting trace to out/test_trace_plot.eps")
-sensors_trace.plot_trace("out/test_trace_plot.eps")
+print(" - Plotting trace to " + this_dirctory + "out/test_trace_plot.eps")
+sensors_trace.plot_trace(this_dirctory + "out/test_trace_plot.eps")
 
 print(" - Saving trace to out/test_trace.txt")
-sensors_trace.save_trace("out/test_trace.txt")
+sensors_trace.save_trace(this_dirctory + "out/test_trace.txt")
 
 print(" - Loading trace from out/test_trace.txt")
-file_sensors_trace = Trace(load_file = "out/test_trace.txt")
+file_sensors_trace = Trace(load_file = this_dirctory + "out/test_trace.txt")
 
 test_result = True
 print(" - Comparing traces")
