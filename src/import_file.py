3882b3f3e3e588a5a596998ddd30a32db4594d82
idiasdas <igordias.ti@gmail.com>
2023-08-10 20:30:48 +0200

    Added execpetions management on import file

--- src/import_file.py
+++ src/import_file.py
@@ -1,6 +1,13 @@
 
 """All packages imported in the project."""
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import collections  as mc
+try:
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib import collections  as mc
+except:
+    raise Exception('Please install requirements.txt. Use one of the following options:\n\t $ pip install -r requirements.txt \n\t $ conda env create -f environment.yml')
+    
+try:
+    import cplex
+except:
+    raise Exception('Please install cplex and add it to PYTHONPATH. See https://www.ibm.com/docs/en/icos/20.1.0?topic=cplex-setting-up-python-api')
