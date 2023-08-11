8e98d646374860d7ebb109822ac22e4aa5538eac
idiasdas <igordias.ti@gmail.com>
2023-08-11 11:28:03 +0200

    Added model_name variable and documented it

--- src/milp_model.py
+++ src/milp_model.py
@@ -1,7 +1,7 @@
 from import_file import *
 
 class MILPModel:
-    def __init__(self, n_available_drones, observation_period, time_step_delta, communication_range, coverage_range, input_graph):
+    def __init__(self, n_available_drones, observation_period, time_step_delta, communication_range, coverage_range, input_graph, model_name = "MILP_Model"):
         """Builds the linear program to obtain the optimal deployment of drones to cover all targets at all time steps.
 
         Args:
@@ -11,6 +11,7 @@ class MILPModel:
             communication_range (float): The range of drones communication.
             coverage_range (float): The maximmum distance for communication between a drone and a target.
             input_graph (Graph): The topology of the problem with the set of deployment positions and targets coordinates at each time step.
+            model_name (str, optional): Name of the cplex model. Defaults to "MILP_Model".
         """        
 
         self.n_available_drones = n_available_drones
@@ -20,3 +21,5 @@ class MILPModel:
         self.coverage_range = coverage_range
         self.input_graph = input_graph
 
+        self.cplex_model = cplex.Cplex()
+        self.cplex_model.set_problem_name(model_name)
