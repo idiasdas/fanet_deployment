9c80949e327b900f1ca47218e64ee4c9f6507683
idiasdas <igordias.ti@gmail.com>
2023-08-11 11:40:25 +0200

    Created define_variable
    Defines a variable. And saves its information in the corresponding
    lists. This function does not add the variables to the cplex model.
    The reason I'm using these lists is because addign variables and
    constraints in batches is faster than adding them one by one for some
    reason.
    
    Args:
        var_name (str): Name of the variable.
        var_lb (float): Lower bound of the variable.
        var_up (float): Upper bound of the variable.
        var_type (str): Type of the variable.

--- src/milp_model.py
+++ src/milp_model.py
@@ -34,3 +34,17 @@ class MILPModel:
         self.constraints_sense = []
         self.constraints_right_hand_side = []
 
+        def define_variable(self, var_name, var_lb, var_up, var_type):
+            """Defines a variable. And saves its information in the corresponding lists. This function does not add the variables to the cplex model.The reason I'm using these lists is because addign variables and constraints in batches is faster than adding them one by one for some reason.
+
+            Args:
+                var_name (str): Name of the variable.
+                var_lb (float): Lower bound of the variable.
+                var_up (float): Upper bound of the variable.
+                var_type (str): Type of the variable.
+            """            
+            self.variables_names.append(var_name)
+            self.variables_lower_bounds.append(var_lb)
+            self.variables_upper_bounds.append(var_up)
+            self.variables_types.append(var_type)
+
