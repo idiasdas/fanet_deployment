54a9890cec2c1583ab712871202f2fda461d651f
idiasdas <igordias.ti@gmail.com>
2023-08-11 17:17:11 +0200

    Now communication range and coverage range are atibutes in the class so the methods can be called without specifiying them.
    And now the grid of positions is built on initialization of the class.

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,6 @@
 from import_file import *
 class Graph:
-    def __init__(self, size_A, heights, base_station, n_drones, n_positions_per_axis, observation_period, targets_trace = []):
+    def __init__(self, size_A, heights, base_station, n_drones, n_positions_per_axis, observation_period, communication_range, coverage_range, targets_trace = []):
         """Describes the graph form which we build the linear programs.
 
         Args:
@@ -10,6 +10,8 @@ class Graph:
             n_drones (int): Amount of drones available
             n_positions_per_axis (int): How many slices we want to divide the area into for each axis. The resulting grid will have n_positions_per_axis^2 positions.
             observation_period (int): Amount of time steps
+            communication_range (float): Maximum distance for communications drone to drone and base to drone 
+            coverage_range (float): Maximum distance for communications drone to target
             targets_trace (list, optional): The coordinates of each sensor for each time step. Defaults to [].
         """
         self.size_A = size_A
@@ -18,13 +20,17 @@ class Graph:
         self.n_drones = n_drones
         self.n_positions_per_axis = n_positions_per_axis
         self.observation_period = observation_period
+        self.communication_range = communication_range
+        self.coverage_range = coverage_range
         self.targets_trace = targets_trace
+
+        self.set_position_grid()
 
     def set_position_grid(self):
         """ Sets the set P as all the positions in the grid inside the area A."""
         self.deployment_positions = np.array([(x, y, z) for x in np.arange(0, self.size_A, self.size_A/(self.n_positions_per_axis + 1)) for y in np.arange(0, self.size_A, self.size_A/(self.n_positions_per_axis + 1)) for z in self.heights if x!= 0 and y != 0])
 
-    def get_target_coverage(self, target_position, coverage_range):
+    def get_target_coverage(self, target_position):
         """Returns the set of positions that cover the target position.
 
         Args:
@@ -34,9 +40,9 @@ class Graph:
         Returns:
             list: List of positions that cover the target position
         """
-        return [position for position in self.deployment_positions if np.linalg.norm(np.array(position) - np.array(target_position)) <= coverage_range]
+        return [position for position in self.deployment_positions if np.linalg.norm(np.array(position) - np.array(target_position)) <= self.coverage_range]
     
-    def get_positions_in_comm_range(self, position, communication_range):
+    def get_positions_in_comm_range(self, position):
         """Returns the set of positions that are in communication range with the given position.
 
         Args:
