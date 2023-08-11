0ab7eca327800bd03a087764373c8be5958292eb
idiasdas <igordias.ti@gmail.com>
2023-08-11 18:04:24 +0200

    Fixed nested methods in __init__
    I had made an extra tab for all the methods in the class.

--- src/milp_model.py
+++ src/milp_model.py
@@ -34,103 +34,103 @@ class MILPModel:
         self.constraints_sense = []
         self.constraints_right_hand_side = []
 
-        def define_variable(self, var_name, var_lb, var_up, var_type):
-            """Defines a variable and saves its information in the corresponding lists. This function does not add the variables to the cplex model. I'm using these lists is because addign variables and constraints in batches is faster than adding them one by one for some reason. 
-            Args:
-                var_name (str): Name of the variable.
-                var_lb (float): Lower bound of the variable.
-                var_up (float): Upper bound of the variable.
-                var_type (str): Type of the variable.
-            """            
-            self.variables_names.append(var_name)
-            self.variables_lower_bounds.append(var_lb)
-            self.variables_upper_bounds.append(var_up)
-            self.variables_types.append(var_type)
+    def define_variable(self, var_name, var_lb, var_up, var_type):
+        """Defines a variable and saves its information in the corresponding lists. This function does not add the variables to the cplex model. I'm using these lists is because addign variables and constraints in batches is faster than adding them one by one for some reason. 
+        Args:
+            var_name (str): Name of the variable.
+            var_lb (float): Lower bound of the variable.
+            var_up (float): Upper bound of the variable.
+            var_type (str): Type of the variable.
+        """            
+        self.variables_names.append(var_name)
+        self.variables_lower_bounds.append(var_lb)
+        self.variables_upper_bounds.append(var_up)
+        self.variables_types.append(var_type)
 
-        def var_z_t_p(self, time_step, position):
-            """Returns the name of the variable z_t_p. This is a binary variable for p \in P and t \in T that says if a drone is deployed at position p at time step t. For the base station this variable is an integer since the base station can have many drones in it simultaneously.
-            
-            Args:
-                time_step (int): Time step.
-                position (tuple): Position coordinates.
-            """
-            return "z_t_"+str(time_step)+"_p_" + str(position)
-
-        def var_z_t_drone_p(self, time_step, drone, position):
-            """Returns the name of the variable z_t_drone_p. This is a binary variable for p \in P and t \in T and drone \in n_available_drones that says if drone is deployed at position p at time step t.
-            
-            Args:
-                time_step (int): Time step.
-                drone (int): Drone index.
-                position (tuple): Position coordinates.
-            """
-            return "z_t_"+str(time_step)+"_drone_"+str(drone)+"_p_"+str(position)
-
-        def var_f_t_p_q(self, time_step, position_p, position_q):
-            """Returns the name of the variable f_t_p_q. This is a continuous variable for p,q \in P and t \in T that says how much flow is sent from position p to position q at time step t.
-
-            Args:
-                time_step (int): Time step.
-                position_p (tuple): Position coordinates.
-                position_q (tuple): Position coordinates.
-            """
-            return "f_t_"+str(time_step)+"_p_"+str(position_p)+"_q_"+str(position_q)
-
-        def define_all_variables(self):
-            """Defines all the variables of the linear program."""
-            # Defining the variables z_t_p for all t \in T and p \in P \cup {base_station}
-            for t in range(self.observation_period):
-                define_variable(self.var_z_t_p(t,self.input_graph.base_station),0,self.n_available_drones,"I")
-                for p in self.input_graph.deployment_positions:
-                    define_variable(self.var_z_t_p(t,p),0,1,"B")
-            
-            # Defining the variables z_t_drone_p for all t \in T, drone \in n_available_drones and p \in P \cup {base_station}
-            for t in range(self.observation_period):
-                for drone in range(self.n_available_drones):
-                    define_variable(self.var_z_t_drone_p(t,drone,self.input_graph.base_station),0,1,"B")
-                    for p in self.input_graph.deployment_positions:
-                        define_variable(self.var_z_t_drone_p(t,drone,p),0,1,"B")
-
-            # Defining the flow variables f_t_base_p for all t \in T and p \in P
-            for t in range(self.observation_period):
-                for p in self.input_graph.get_positions_in_comm_range(self.input_graph.base_station):
-                    define_variable(self.var_f_t_p_q(t,self.input_graph.base_station,p),0,len(self.input_graph.targets_trace),"C")
-
-            # Defining the flow variables f_t_p_q for all t \in T, p,q \in P and p \neq q
-            for t in range(self.observation_period):
-                for p in self.input_graph.deployment_positions:
-                    for q in self.input_graph.get_positions_in_comm_range(p):
-                        define_variable(self.var_f_t_p_q(t,p,q),0,len(self.input_graph.targets_trace),"C")
-
-        def define_constraint(self, constr_name, constr_linear_expr, constr_sense, constr_rhs):
-            """Defines a constraint and saves its information in the corresponding lists. This function does not add the constraints to the cplex model. I'm using these lists is because addign variables and constraints in batches is faster than adding them one by one for some reason.
-
-            Args:
-                constr_name (str): Name of the constraint.
-                constr_linear_expr (cplex.SparsePair): Linear expression of the constraint.
-                constr_sense (str): Sense of the constraint.
-                constr_rhs (float): Right hand side of the constraint.
-            """            
-            self.constraints_names.append(constr_name)
-            self.constraints_linear_expr.append(constr_linear_expr)
-            self.constraints_sense.append(constr_sense)
-            self.constraints_right_hand_side.append(constr_rhs)
-
-        def set_variables_to_cplex(self):
-            """Adds the variables to the cplex model."""
-            self.cplex_model.variables.add(names = self.variables_names, lb = self.variables_lower_bounds, ub = self.variables_upper_bounds, types = self.variables_types)
-
-        def set_constraints_to_cplex(self):
-            """Adds the constraints to the cplex model."""
-            self.cplex_model.linear_constraints.add(lin_expr = self.constraints_linear_expr, senses = self.constraints_sense, rhs = self.constraints_right_hand_side, names = self.constraints_names)
-
-        def set_objective_function(self, objective_function, maximize = True):
-            """Sets the objective function of the cplex model.
-
-            Args:
-                objective_function (list): List of tuples describing the objective function. Each tuple has the form (variable_name (str), coefficient (float)).
-                maximize (bool, optional): If True, the objective function is maximized. If False, the objective function is minimized. Defaults to True.
-            """            
-            self.cplex_model.objective.set_linear(objective_function)
-            self.cplex_model.objective.set_sense(self.cplex_model.objective.sense.maximize if maximize else self.cplex_model.objective.sense.minimize)
+    def var_z_t_p(self, time_step, position):
+        """Returns the name of the variable z_t_p. This is a binary variable for p \in P and t \in T that says if a drone is deployed at position p at time step t. For the base station this variable is an integer since the base station can have many drones in it simultaneously.
         
+        Args:
+            time_step (int): Time step.
+            position (tuple): Position coordinates.
+        """
+        return "z_t_"+str(time_step)+"_p_" + str(position)
+
+    def var_z_t_drone_p(self, time_step, drone, position):
+        """Returns the name of the variable z_t_drone_p. This is a binary variable for p \in P and t \in T and drone \in n_available_drones that says if drone is deployed at position p at time step t.
+        
+        Args:
+            time_step (int): Time step.
+            drone (int): Drone index.
+            position (tuple): Position coordinates.
+        """
+        return "z_t_"+str(time_step)+"_drone_"+str(drone)+"_p_"+str(position)
+
+    def var_f_t_p_q(self, time_step, position_p, position_q):
+        """Returns the name of the variable f_t_p_q. This is a continuous variable for p,q \in P and t \in T that says how much flow is sent from position p to position q at time step t.
+
+        Args:
+            time_step (int): Time step.
+            position_p (tuple): Position coordinates.
+            position_q (tuple): Position coordinates.
+        """
+        return "f_t_"+str(time_step)+"_p_"+str(position_p)+"_q_"+str(position_q)
+
+    def define_all_variables(self):
+        """Defines all the variables of the linear program."""
+        # Defining the variables z_t_p for all t \in T and p \in P \cup {base_station}
+        for t in range(self.observation_period):
+            define_variable(self.var_z_t_p(t,self.input_graph.base_station),0,self.n_available_drones,"I")
+            for p in self.input_graph.deployment_positions:
+                define_variable(self.var_z_t_p(t,p),0,1,"B")
+        
+        # Defining the variables z_t_drone_p for all t \in T, drone \in n_available_drones and p \in P \cup {base_station}
+        for t in range(self.observation_period):
+            for drone in range(self.n_available_drones):
+                define_variable(self.var_z_t_drone_p(t,drone,self.input_graph.base_station),0,1,"B")
+                for p in self.input_graph.deployment_positions:
+                    define_variable(self.var_z_t_drone_p(t,drone,p),0,1,"B")
+
+        # Defining the flow variables f_t_base_p for all t \in T and p \in P
+        for t in range(self.observation_period):
+            for p in self.input_graph.get_positions_in_comm_range(self.input_graph.base_station):
+                define_variable(self.var_f_t_p_q(t,self.input_graph.base_station,p),0,len(self.input_graph.targets_trace),"C")
+
+        # Defining the flow variables f_t_p_q for all t \in T, p,q \in P and p \neq q
+        for t in range(self.observation_period):
+            for p in self.input_graph.deployment_positions:
+                for q in self.input_graph.get_positions_in_comm_range(p):
+                    define_variable(self.var_f_t_p_q(t,p,q),0,len(self.input_graph.targets_trace),"C")
+
+    def define_constraint(self, constr_name, constr_linear_expr, constr_sense, constr_rhs):
+        """Defines a constraint and saves its information in the corresponding lists. This function does not add the constraints to the cplex model. I'm using these lists is because addign variables and constraints in batches is faster than adding them one by one for some reason.
+
+        Args:
+            constr_name (str): Name of the constraint.
+            constr_linear_expr (cplex.SparsePair): Linear expression of the constraint.
+            constr_sense (str): Sense of the constraint.
+            constr_rhs (float): Right hand side of the constraint.
+        """            
+        self.constraints_names.append(constr_name)
+        self.constraints_linear_expr.append(constr_linear_expr)
+        self.constraints_sense.append(constr_sense)
+        self.constraints_right_hand_side.append(constr_rhs)
+
+    def set_variables_to_cplex(self):
+        """Adds the variables to the cplex model."""
+        self.cplex_model.variables.add(names = self.variables_names, lb = self.variables_lower_bounds, ub = self.variables_upper_bounds, types = self.variables_types)
+
+    def set_constraints_to_cplex(self):
+        """Adds the constraints to the cplex model."""
+        self.cplex_model.linear_constraints.add(lin_expr = self.constraints_linear_expr, senses = self.constraints_sense, rhs = self.constraints_right_hand_side, names = self.constraints_names)
+
+    def set_objective_function(self, objective_function, maximize = True):
+        """Sets the objective function of the cplex model.
+
+        Args:
+            objective_function (list): List of tuples describing the objective function. Each tuple has the form (variable_name (str), coefficient (float)).
+            maximize (bool, optional): If True, the objective function is maximized. If False, the objective function is minimized. Defaults to True.
+        """            
+        self.cplex_model.objective.set_linear(objective_function)
+        self.cplex_model.objective.set_sense(self.cplex_model.objective.sense.maximize if maximize else self.cplex_model.objective.sense.minimize)
+    
