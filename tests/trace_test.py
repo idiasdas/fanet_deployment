36cd1a73578213718112a171944045ddbbbce5d8
idiasdas <igordias.ti@gmail.com>
2023-08-16 16:04:43 +0200

    Removed import form import_file

--- tests/trace_test.py
+++ tests/trace_test.py
@@ -1,7 +1,6 @@
 import os
 import sys
 sys.path.append(os.path.dirname(__file__) + '/../src/')  # Now can import modules in src
-from import_file import *
 from targets_trace import Trace
 
 print(" - Testing Trace class")
