e44f2619e6426a6109e03352455bad60ee1820d8
idiasdas <igordias.ti@gmail.com>
2023-08-17 17:15:39 +0200

    Added modules import plus variables

--- tests/trace_test.py
+++ tests/trace_test.py
@@ -1,11 +1,19 @@
 import os
 import sys
 this_dirctory = os.path.dirname(__file__)
-sys.path.append( this_dirctory+ '/../src/')  # Now can import modules in src
+sys.path.append( this_dirctory + '/../src/')  # Now can import modules in src
 from targets_trace import Trace
+from graph import Graph
+import numpy as np
 
 print(" - Testing Trace class")
-sensors_trace = Trace(5,3,20,100)
+
+n_targets = 5
+observation_period = 3
+target_speed = 20
+area_size = 100
+
+sensors_trace = Trace(n_targets, observation_period, target_speed, area_size)
 out_txt = "\t* {variable:20} = {test_value:<10}"
 print(" - Created new instante:")
 print(out_txt.format(variable = "n_targets", test_value = str(sensors_trace.n_targets)))
@@ -39,3 +47,25 @@ print(out_txt.format(variable = "trace_set", test_value = str(sensors_trace.trac
 test_result = test_result and sensors_trace.trace_set == file_sensors_trace.trace_set
 
 print(" TEST RESULT = "  + ("OK" if str(test_result) else "FAIL"))
+
+print(" NOW TESTING CREATION OF FEASIBLE TRACES")
+
+my_graph = Graph(size_A = 100, heights = [45], base_station = (0,0,0), n_positions_per_axis = 3, communication_range = 40, coverage_tan_angle = np.tan(np.pi/6))
+
+
+
+n_traces = 5
+count_failed_traces = 0
+for n in range(n_traces):
+    new_trace = Trace(n_targets, observation_period, target_speed, area_size)
+    while not my_graph.verify_trace_feasiblity(new_trace.trace_set):
+        count_failed_traces += 1
+        new_trace = Trace(n_targets, observation_period, target_speed, area_size)
+    print(" - Trace " + str(n) + " is feasible")
+    new_trace.save_trace(this_dirctory + "/out/feasible_trace_" + str(n) + ".txt")
+    new_trace.plot_trace(this_dirctory + "/out/feasible_trace_" + str(n) + ".eps")
+print(" - Done testing creation of feasible traces")
+print(" - " + str(count_failed_traces) + " traces were not feasible")
+
+
+
