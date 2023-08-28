d1f452893593da827190caff094d72659ce40680
idiasdas <igordias.ti@gmail.com>
2023-08-28 23:55:18 +0200

    Fixed BASE_DIR. Removed EXPERIMENT_NAME -> Now defined in PARAMETERS

--- fanet/setup/config.py
+++ fanet/setup/config.py
@@ -3,10 +3,8 @@ from fanet.setup.parameters import DEFAULT_PARAMETERS, TEST_PARAMETERS
 
 # Parameters used in the project.
 PARAMETERS = TEST_PARAMETERS
-# Name of experiement. Used to name the output files.
-EXPERIMENT_NAME = "test"
 # Path to the root directory of the project
-BASE_DIR = os.path.dirname(os.path.dirname(__file__)) + "/"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + "/"
 # Path where tests results are stored
 TESTS_OUTPUT_DIR = os.path.join(BASE_DIR, "tests", "out") + "/"
 # Path where files are stored such as traces, solutions and figures.
