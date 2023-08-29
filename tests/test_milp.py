2ec1da5fd8cbc41af0c0770f4acfcf67535c25d6
idiasdas <igordias.ti@gmail.com>
2023-08-29 15:38:45 +0200

    defined example scenarios as external methods and updated tests accordingly

--- tests/test_milp.py
+++ tests/test_milp.py
@@ -6,13 +6,11 @@ import numpy as np
 import os
 this_dirctory = os.path.dirname(__file__)
 
-
-def test_basic_0() -> None:
+def example_basic_0() -> list:
     """For this example, we have one sensor in the middle of the area (50,50,0) and only one deployement solution (50,50,10) for a single time step.
 
-    This test verifies if the model is built properly, solved and if the solution is correct.
-
-    The expected solution is: 142.82857. This is the distance cost for the drone to leave the base station, go to (50,50,10) and go back to the base station.
+    Returns:
+        list: [targets_trace, graph, milp_model]
     """
     targets_trace = TargetsTrace(n_targets=1, observation_period=1)
     targets_trace.trace_set = [[(50, 50)]]
@@ -23,19 +21,15 @@ def test_basic_0() -> None:
                            targets_trace=targets_trace,
                            input_graph=graph,
                            alpha=0,
-                           beta=0)
+                           beta=0.08095)
+    return [targets_trace, graph, milp_model]
 
-    milp_model.model_shut_up()
-    milp_model.build_model()
-    milp_model.solve_model()
-    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
-    assert round(milp_model.get_objective_value(), 5) == 142.82857
-    assert round(milp_model.get_solution_distance(), 5) == 142.82857
-    milp_model.cplex_finish()
+def example_basic_1() -> list:
+    """In this example, we have two time steps, where the sensor remains in (50,50,0). So the drone is expected to go to (50,50,10) for both time steps.
 
-
-def test_basic_1() -> None:
-    """In this example, we have two time steps, where the sensor remains in (50,50,0). So the drone is expected to go to (50,50,10) for both time steps."""
+    Returns:
+        list: [targets_trace, graph, milp_model]
+    """
     targets_trace = TargetsTrace(n_targets=1, observation_period=2)
     targets_trace.trace_set = [[(50, 50), (50, 50)]]
     graph = Graph(100, [10], (0, 0, 0), 1, 100, np.tan(np.pi/6))
@@ -45,57 +39,10 @@ def test_basic_1() -> None:
                            targets_trace=targets_trace,
                            input_graph=graph,
                            alpha=0,
-                           beta=0)
+                           beta=0.08095)
+    return [targets_trace, graph, milp_model]
 
-    milp_model.model_shut_up()
-    milp_model.build_model()
-    milp_model.solve_model()
-
-    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
-    assert round(milp_model.get_objective_value(), 5) == 142.82857
-    milp_model.cplex_finish()
-
-
-def test_save_file() -> None:
-    """This test verifies if the method save_solution_to_file is working properly.
-    """
-    targets_trace = TargetsTrace(n_targets=1, observation_period=1)
-    targets_trace.trace_set = [[(50, 50)]]
-    graph = Graph(100, [10], (0, 0, 0), 1, 100, np.tan(np.pi/6))
-    milp_model = MilpModel(n_available_drones=1,
-                           observation_period=1,
-                           time_step_delta=1,
-                           targets_trace=targets_trace,
-                           input_graph=graph,
-                           alpha=0,
-                           beta=0)
-
-    milp_model.model_shut_up()
-    milp_model.build_model()
-    milp_model.solve_model()
-    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
-
-    out_file = this_dirctory + "/out/test_basic_0"
-
-    assert not os.path.exists(out_file)
-
-    milp_model.cplex_model.write(out_file + ".lp")
-    milp_model.cplex_save_solution(out_file + ".sol")
-    assert os.path.exists(out_file + ".sol")
-
-    os.remove(out_file + ".sol")
-    assert not os.path.exists(out_file + ".sol")
-
-    milp_model.save_solution(out_file + ".sol")
-    assert os.path.exists(out_file + ".sol")
-
-    os.remove(out_file + ".sol")
-    assert not os.path.exists(out_file + ".sol")
-
-    milp_model.cplex_finish()
-
-
-def test_movement_0() -> None:
+def example_movement_0() -> list:
     """In this example, we have two time steps, where the sensor moves from (25,50,0) to (75,50,0). So the drone is expected to go to move between time steps."""
     targets_trace = TargetsTrace(n_targets=1, observation_period=2)
     targets_trace.trace_set = [[(25, 50), (75, 50)]]
@@ -107,20 +54,11 @@ def test_movement_0() -> None:
                            targets_trace=targets_trace,
                            input_graph=graph,
                            alpha=0,
-                           beta=0)
+                           beta=0.08095)
+    return [targets_trace, graph, milp_model]
 
-    milp_model.model_shut_up()
-
-    milp_model.build_model()
-    milp_model.solve_model()
-
-    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
-    assert round(milp_model.get_objective_value(), 5) == 197.48087
-    milp_model.cplex_finish()
-
-
-def test_movement_1() -> None:
-    """In this example, we have two time steps, where the sensor moves from (25,50,0) to (75,50,0). So the drone is expected to go to move between time steps. Unlike test_movement_0, we have only 60m of communication range. So this should be infeasible."""
+def example_movement_1() -> list:
+    """In this example, we have two time steps, where the sensor moves from (25,50,0) to (75,50,0). But only one drone is available with 60m communication range. So the solution is infeasible."""
     targets_trace = TargetsTrace(n_targets=1, observation_period=2)
     targets_trace.trace_set = [[(25, 50), (75, 50)]]
     graph = Graph(100, [10], (0, 0, 0), 1, 60, np.tan(np.pi/6))
@@ -131,21 +69,11 @@ def test_movement_1() -> None:
                            targets_trace=targets_trace,
                            input_graph=graph,
                            alpha=0,
-                           beta=0)
+                           beta=0.08095)
+    return [targets_trace, graph, milp_model]
 
-    milp_model.model_shut_up()
-
-    milp_model.build_model()
-    milp_model.solve_model()
-
-    assert milp_model.cplex_model.solution.get_status() == INFEASIBLE_SOLUTION
-    assert milp_model.get_objective_value() == -1
-    milp_model.cplex_finish()
-
-
-def test_movement_2() -> None:
-    """In this example, we have two time steps, where the sensor moves from (25,50,0) to (75,50,0). So the drone is expected to go to move between time steps. Unlike test_movement_0, we have only 60m of communication range but 2 drones available. So at the second time step two drones should be deployed for the flow to reach the sensor."""
-
+def example_movement_2() -> list:
+    """In this example, we have two time steps, where the sensor moves from (25,50,0) to (75,50,0). So the drone is expected to go to move between time steps. Unlike test_movement_0, we have only 60m of communication range but 2 drones available."""
     targets_trace = TargetsTrace(n_targets=1, observation_period=2)
     targets_trace.trace_set = [[(25, 50), (75, 50)]]
     graph = Graph(100, [10], (0, 0, 0), 1, 60, np.tan(np.pi/6))
@@ -156,13 +84,85 @@ def test_movement_2() -> None:
                            targets_trace=targets_trace,
                            input_graph=graph,
                            alpha=0,
-                           beta=0)
+                           beta=0.08095)
+    return [targets_trace, graph, milp_model]
 
+def example_sensor_coverage_0() -> list:
+    """Two drones available. Two deployment positions: (25,50,10) and (75,50,10). Two time steps. Two sensors: (25,50) and (75,50). Both drones must be deployed to cover the sensors."""
+    targets_trace = TargetsTrace(n_targets=2, observation_period=2)
+    targets_trace.trace_set = [[(25, 50), (25, 50)], [(75, 50), (75, 50)]]
+    graph = Graph(100, [10], (0, 0, 0), 1, 100, np.tan(np.pi/6))
+    graph.deployment_positions = [(25, 50, 10), (75, 50, 10)]
+    milp_model = MilpModel(n_available_drones=2,
+                           observation_period=2,
+                           time_step_delta=1,
+                           targets_trace=targets_trace,
+                           input_graph=graph,
+                           alpha=0,
+                           beta=0.08095)
+    return [targets_trace, graph, milp_model]
+
+def test_basic_0() -> None:
+    """Verifies if the model properly solves a problem with 1 time step. Where the drone is forced to follows the trajectory: base_station -> (50,50,10) -> base_station.
+    """
+    targets_trace, graph, milp_model = example_basic_0()
     milp_model.model_shut_up()
+    milp_model.build_model()
+    milp_model.solve_model()
+    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
+    assert round(milp_model.get_objective_value(), 5) == 142.82857
+    milp_model.cplex_finish()
 
+
+def test_basic_1() -> None:
+    """Tests if the model properly solves a problem with 2 time steps."""
+    targets_trace, graph, milp_model = example_basic_1()
+    milp_model.model_shut_up()
+    milp_model.build_model()
+    milp_model.solve_model()
+    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
+    assert round(milp_model.get_objective_value(), 5) == 142.82857
+    milp_model.cplex_finish()
+
+
+def test_save_file() -> None:
+    """This test verifies if the method save_solution_to_file is working properly.
+    """
+    targets_trace, graph, milp_model = example_basic_0()
+    milp_model.model_shut_up()
     milp_model.build_model()
     milp_model.solve_model()
 
+
+def test_movement_0() -> None:
+    """Tests if milp models properly solves example_movement_0 where the drones can move between time steps."""
+
+    targets_trace, graph, milp_model = example_movement_0()
+    milp_model.model_shut_up()
+    milp_model.build_model()
+    milp_model.solve_model()
+    assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
+    assert round(milp_model.get_objective_value(), 5) == 197.48087
+    milp_model.cplex_finish()
+
+
+def test_movement_1() -> None:
+    """The drone is expected to go to move between time steps. Unlike test_movement_0, we have only 60m of communication range. So this should be infeasible."""
+    targets_trace, graph, milp_model = example_movement_1()
+    milp_model.model_shut_up()
+    milp_model.build_model()
+    milp_model.solve_model()
+    assert milp_model.cplex_model.solution.get_status() == INFEASIBLE_SOLUTION
+    assert milp_model.get_objective_value() == -1
+    milp_model.cplex_finish()
+
+
+def test_movement_2() -> None:
+    """Tests if milp_model properly solves example_movement_2."""
+    targets_trace, graph, milp_model = example_movement_2()
+    milp_model.model_shut_up()
+    milp_model.build_model()
+    milp_model.solve_model()
     assert milp_model.cplex_model.solution.get_status() == OPTIMAL_SOLUTION
     assert round(milp_model.get_objective_value(), 5) == 294.96174
     milp_model.cplex_finish()
